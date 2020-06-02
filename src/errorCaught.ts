c3fc11587c56dfc9c03461d5a12ee0dcea5ded89
wyp <593704361@qq.com>
2020-06-02 23:26:24 +0800

    feat:resourceErrorCatch && syncErrorCatch

--- src/errorCaught.ts
+++ src/errorCaught.ts
@@ -1,40 +1,85 @@
+/**
+ * 错误处理类 ErrorCaught
+ * 处理资源请求错误 resourceErrorCatch
+ * 
+ */
 interface ICaughtmsg {
     colSource: boolean;
 }
-class ErrorCaught{
+class ErrorCaught {
+    colSource: boolean;
     constructor(options: ICaughtmsg) {
+        const { colSource } = options;
+        this.colSource = colSource;
         this._init()
     }
     private _init(): void {
-        this.resourceErrorCatch()
+        if (this.colSource) {
+            this.resourceErrorCatch()
+        }
+        this.syncErrorCatch()
     }
     /**
      * 资源请求出错，400，500类型错误
+     * 图片资源，js脚本，css资源
+     * colSource选项决定是否捕获
      */
     resourceErrorCatch(): void {
-        if(window.addEventListener) {
+        if (window.addEventListener) {
             window.addEventListener('error', (event) => {
                 var node: EventTarget = event.target ? event.target : event.srcElement;
                 var outerHTML = (node as Element).outerHTML;
-                // var e = {
-                //     type: "resourceError",
-                //     target: {
-                //         outerHTML: outerHTML,
-                //         src: node && (node).src,
-                //         tagName: node && (node as Element).tagName,
-                //         id: node && (node as Element).id,
-                //         className: node && node.className,
-                //         name: node && node.name,
-                //         type: node && (node as HTMLLinkElement).type,
-                //         timeStamp: event.timeStamp
-                //     }
-                // }
-                // console.log(e.src)
+                // 图片资源，js脚本，css资源
+                if (/img|script|link/.test((node as Element).localName)) {
+                    var e = {
+                        type: "resourceError",
+                        target: {
+                            outerHTML: outerHTML,
+                            src: node && (node as HTMLImageElement).src,
+                            tagName: node && (node as Element).localName,
+                            id: node && (node as Element).id,
+                            className: node && (node as Element).className,
+                            name: node && (node as HTMLImageElement).name,
+                            type: node && (node as HTMLLinkElement).type,
+                            timeStamp: event.timeStamp
+                            // status: ?
+                            // statusText ?
+                        }
+                    }
+                    console.log(e)
+                }
+
                 return true
             }, true)
         }
     }
-    // 异常捕获
+    /**
+     * 1.异步错误捕获
+     * 2.跨域脚本错误
+     *  2.1 cross-origin="anonymous"
+     *  2.2 后台允许跨域
+     */
+    syncErrorCatch(): void {
+        window.onerror = function (msg: string | Event, url: string, row: number, col: number, error: Error) {
+            console.log(msg, url, row, col, error)
+            console.log(arguments)
+            console.log(arguments.callee)
+            // console.log(arguments.callee.caller)
+            // console.log(arguments.callee.caller.caller)
+            var result = {
+                message: error && error.message,
+                row: row,
+                col: col,
+                // fileName: "",
+                name: error && error.name || "uncaught error",
+                stacktrace: error && error.stack,
+                type: "uncaught"
+            };
+            console.log(result)
+            return true
+        }
+    }
 }
 
 export default ErrorCaught
+
--- test/app.js
+++ test/app.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = new express()
 const path = require('path')
-app.use(express.static(path.join(__dirname, '../dist')))
 
-
-app.listen(3001, () => { console.log('3001')})
+let options = {
+    setHeaders: function (res, path, stat) {
+        res.set('Access-Control-Allow-Origin', '*')
+    }
+}
+app.all('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+app.use(express.static(path.join(__dirname, '../dist'), options))
+app.listen(3001, () => { console.log('3001') })
